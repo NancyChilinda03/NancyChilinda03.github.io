2ae8d8bc7414c68db505f5f2f2a2aab5a4fcbd52
NancyChilinda03 <nancychilinda11@gmail.com>
2026-05-26 16:50:47 +0200

    Configure GitHub Pages deployment and base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+      base: '/NancyChilinda03',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
